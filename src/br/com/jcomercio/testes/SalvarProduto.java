60272168e99f95b2e170074119283096633fcf60
Antonio Alves <antonioaugusto.alves1981@gmail.com>
2022-11-10 16:30:08 -0300

    Testando um array de produtos

---
+++ src/br/com/jcomercio/testes/SalvarProduto.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.jcomercio.testes;
+
+/**
+ *
+ * @author ANTONIO
+ */
+public class SalvarProduto {
+    public static void main(String[] args) {
+        String[] lista = new String[5];
+        lista[0] = "Bom dia";
+        lista[1] = "Boa Tarde";
+        lista[2] = "Boa Noite";
+        lista[3] = "Com licença";
+        lista[4] = "Muito obrigado";
+    }
+    
+}
